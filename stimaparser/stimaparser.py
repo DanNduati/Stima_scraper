47410844089a23bca94bdc4abb99486605376147
DanielNduati <dannduati2@gmail.com>
2022-07-06 23:49:41 +0300

    Basic text extraction from all pdf documents with pdfminer

--- stimaparser/stimaparser.py
+++ stimaparser/stimaparser.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
 """Stimaparser"""
+
+from pdfminer.high_level import extract_text
+from pathlib import Path
+
+PDF_FILE_PATH = Path(__file__).parent.parent.absolute().joinpath("pdfs")
+
+
+def extract_pdftext(pdf_path: Path) -> None:
+    text = extract_text(str(pdf_path))
+    print(repr(text))
+
+
+def main():
+    pdf_files = Path(PDF_FILE_PATH).glob("**/*.pdf")
+    for pdf_file in pdf_files:
+        print(f"{'*'*len(str(pdf_file))}")
+        print(pdf_file)
+        print(f"{'*'*len(str(pdf_file))}")
+        text = extract_text(str(pdf_file))
+        print(repr(text))
+
+
+if __name__ == "__main__":
+    main()
